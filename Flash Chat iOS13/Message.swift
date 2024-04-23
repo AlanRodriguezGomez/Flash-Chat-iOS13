e0a92f5cb6b64af470c53f5e8e6f6b9b8e158856
AlanRodriguezGomez <47120912+AlanRodriguezGomez@users.noreply.github.com>
2024-04-22 18:10:42 -0600

    Cambios
    * Se sube proyecto completo.

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Alan Josue Rodriguez Gomez on 17/04/24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+
+struct Message {
+    let sender: String
+    let body: String
+}
